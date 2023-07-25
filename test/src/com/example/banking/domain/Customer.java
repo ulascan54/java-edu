24700961056c5ceb0541bfb06be6c981b023a823
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-25 15:19:20 +0300

    constructure

--- test/src/com/example/banking/domain/Customer.java
+++ test/src/com/example/banking/domain/Customer.java
@@ -1,5 +1,12 @@
 package com.example.banking.domain;
-
-public class Customer {
-
+// alt + shit + S : generate source code
+public class Customer {// Class -> attribute, ii. behavior -> method
+	private String identity; //attribute/state/data
+	private String fullname;
+	private int birthYear;
+	private String email;
+	private String sms;
+	
+	//Constructor -> method -> initializes attributes
+	
 }
