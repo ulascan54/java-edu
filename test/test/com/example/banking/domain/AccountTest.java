4a625aa29c6a425ce5a23d89644bbcf77d7fcada
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-26 11:05:46 +0300

    pparameterized test added

--- test/test/com/example/banking/domain/AccountTest.java
+++ test/test/com/example/banking/domain/AccountTest.java
@@ -3,20 +3,22 @@ package com.example.banking.domain;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-@DisplayName("Functional Test for Account")
+@DisplayName("Functional Test for Account class")
 
 class AccountTest {
 	@DisplayName("creates an account object successfuly")
-	@Test
-	void createAccount() {
+	@ParameterizedTest
+	@CsvFileSource(resources = "/accounts.csv")
+	void createAccount(String iban, double balance) {
 		// 1. Setup/Fixture
 		// 2. Call exercise method
-		Account acc = new Account("tr1", 10_000);
+		Account acc = new Account(iban, balance);
 		// 3. verification
-		assertEquals("tr1", acc.getIban());
-		assertEquals(10_000, acc.getBalance());
+		assertEquals(iban, acc.getIban());
+		assertEquals(balance, acc.getBalance());
 		// 4. tear-down
 		
 	}
