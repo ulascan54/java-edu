cf1ee25330887196b6ed46e7fc31004f4845398f
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-26 16:25:55 +0300

    added some tase case for account

--- test/test/com/example/banking/domain/AccountTest.java
+++ test/test/com/example/banking/domain/AccountTest.java
@@ -1,8 +1,10 @@
 package com.example.banking.domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -20,7 +22,52 @@ class AccountTest {
 		assertEquals(iban, acc.getIban());
 		assertEquals(balance, acc.getBalance());
 		// 4. tear-down
+	}
 
+	@Test
+	@DisplayName("with draw negative amount should return false")
+	void withdrawNegativeAmountShouldReturnFalse() {
+		Account account = new Account("tr1", 10_000);
+		assertFalse(account.withdraw(-1.));
+		assertEquals(10_000, account.getBalance());
+	}
+	@Test
+	@DisplayName("withdraw amount greater than balance should return false")
+	void withdrawOverBalanceShouldReturnFalse() {
+		Account account = new Account("tr1", 10_000);
+		assertFalse(account.withdraw(10_000.1));
+		assertEquals(10_000, account.getBalance());
+	}
+	
+	@Test
+	@DisplayName("withdraw all balance should return true")
+	void withdrawAllBalanceShouldReturnTrue() {
+		Account account = new Account("tr1", 10_000);
+		assertTrue(account.withdraw(10_000));
+		assertEquals(0., account.getBalance());
+	}
+	
+	@Test
+	@DisplayName("deposit with negative amount should return false")
+	void depositNegativeAmountShouldReturnFalse() {
+		Account account = new Account("tr1", 10_000);
+		assertFalse(account.deposit(-1));
+		assertEquals(10_000, account.getBalance());
+	}
+	
+	@Test
+	@DisplayName("deposit positive amount should return true")
+	void depositPositiveAmountShouldReturnTrue() {
+		Account account = new Account("tr1", 10_000);
+		assertTrue(account.deposit(1));
+		assertEquals(10_001, account.getBalance());
+	}
+	
+	@Test
+	@DisplayName("toString() should contain Account")
+	void toStringShouldContainAccount() {
+		Account account = new Account("tr1", 10_000);
+		assertTrue(account.toString().contains("Account"));
 	}
 
 }
