fcc3a5c92162fa43266bf75138136f7e9625225e
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-27 16:41:32 +0300

    moduler programming

---
+++ lottery_module/src/com/example/lottery/service/LotteryService.java
@@ -0,0 +1,9 @@
+package com.example.lottery.service;
+
+import java.util.List;
+
+public interface LotteryService {
+	List<Integer> draw(int max, int length);
+
+	List<List<Integer>> draw(int max, int length, int column);
+}
