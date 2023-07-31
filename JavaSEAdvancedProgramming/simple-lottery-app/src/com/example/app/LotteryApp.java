43d76c2617fc285a2e5aa316bf945f5cf8f235cf
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-31 12:41:57 +0300

    added simple example

---
+++ JavaSEAdvancedProgramming/simple-lottery-app/src/com/example/app/LotteryApp.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class LotteryApp {
+    public static void main(String[] args) {
+        var numbers = ThreadLocalRandom.current().ints(1,60)
+                .distinct()
+                .limit(6)
+                .sorted()
+                .boxed()
+                .toList();
+        System.out.println(numbers);
+
+    }
+}
