02ccc9393e9aa4d9b443962be6a3fb65274bc209
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-02 19:29:37 +0300

    function programming exercise2

--- JavaSEAdvancedProgramming/study-functional-programming/src/com/example/Exercise2.java
+++ JavaSEAdvancedProgramming/study-functional-programming/src/com/example/Exercise2.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import static com.example.LetterGrade.*;
@@ -9,10 +11,16 @@ public class Exercise2 {
     public static void main(String[] args) {
         var students = List.of(
                 new Student("1", "jack bauer", List.of(AA, FF, CB, BB, CB)),
-                new Student("2", "kate austen", List.of(AA, AA, BB, BA, CB)),
+                new Student("2", "kate austen", List.of(AA, AA, BB, AA, CB)),
                 new Student("3", "james sawyer", List.of(AA, FF, FF, BA, BA)),
-                new Student("4", "jin kwon", List.of(AA, AA, BB, BB, CC)));
-
+                new Student("4", "jin kwon", List.of(AA, AA, BB, AA, CC)));
+        Function<Student,Long> extractNumberOfAAs=
+                student -> student.grades().stream().filter(LetterGrade.AA::equals).count();
+        Function<Student,Double> extractAverageGrade=
+                student -> student.grades().stream().mapToInt(LetterGrade::getValue).average().getAsDouble();
+        students.stream()
+                .max(Comparator.comparing(extractNumberOfAAs).thenComparing(extractAverageGrade))
+                .ifPresent(System.out::println);
     }
 }
 
