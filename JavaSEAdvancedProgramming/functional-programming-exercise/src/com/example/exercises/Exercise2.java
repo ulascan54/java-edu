7d4b4bd89ddc1d5d97b1ebecafed84ccd7f13ef7
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-05 11:53:37 +0300

    nio

--- JavaSEAdvancedProgramming/functional-programming-exercise/src/com/example/exercises/Exercise2.java
+++ JavaSEAdvancedProgramming/functional-programming-exercise/src/com/example/exercises/Exercise2.java
@@ -2,6 +2,11 @@ package com.example.exercises;
 
 import com.example.dao.CountryDao;
 import com.example.dao.InMemoryWorldDao;
+import com.example.domain.Country;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 
@@ -13,6 +18,21 @@ public class Exercise2 {
 
 	public static void main(String[] args) {
 		// Find the most populated city of each continent
+		//countryDao.getAllContinents().stream().map(continent -> continent.equals(countryDao.findCountriesByContinent(continent).stream()
+		//		.map(country -> country.getCities().stream().mapToInt(city -> city.getPopulation()).max()))).forEach(System.out::println));
+
+		//var countries = countryDao.findAllCountries();
+		//var mostPopulatedCitiesInEachCountry = countries.stream().map(country -> country.getCities().stream()
+		//		.mapToInt(city -> city.getPopulation()).max());
+		//var continents = countryDao.getAllContinents();
+		//var countriesByContinents = continents.stream().map(continent -> countryDao.findCountriesByContinent(continent)).toList();
+
+		//countriesByContinents.stream().map(countries -> countries.stream().map(country -> country.getCities().stream()
+		//		.collect(Collectors.groupingBy())
+		//));
+
+
+
 	}
 
 }
