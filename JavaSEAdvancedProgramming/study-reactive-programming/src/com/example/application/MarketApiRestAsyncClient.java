87033f5c87784e1f59478d1b1dc95a8a4196f6b4
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-06 18:10:52 +0300

    sync async

--- JavaSEAdvancedProgramming/study-reactive-programming/src/com/example/application/MarketApiRestAsyncClient.java
+++ JavaSEAdvancedProgramming/study-reactive-programming/src/com/example/application/MarketApiRestAsyncClient.java
@@ -5,29 +5,39 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MarketApiRestAsyncClient {
     private static final AtomicInteger counter = new AtomicInteger();
-    private static final String URL = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
+    private static final String URL = "https://api.binance.com/api/v3/ticker/price?symbol=%s";
 
     //Duration: 1037, Average:51.85
     public static void main(String[] args) throws IOException, InterruptedException {
         var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder()
-                .uri(URI.create(URL))
-                .header("Accept", "application/json")
-                .build();
         var start = System.currentTimeMillis();
-        for (var i = 0; i < 20; ++i) {
+        var markets = List.of("BTCUSDT", "ETHBTC", "LTCBTC", "BNBBTC", "NEOBTC", "QTUMETH", "EOSETH", "SNTETH", "BNTETH", "BCCBTC");
+        markets.forEach(symbol -> {
+
+
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create(URL.formatted(symbol)))
+                    .header("Accept", "application/json")
+                    .build();
             client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                     .thenAccept(response -> {
-                        System.out.println(response.body());
-                        if (counter.incrementAndGet() == 20){
+                        var index = counter.incrementAndGet();
+                        System.out.println(index + ": " + response.body());
+                        if (index == markets.size()) {
                             var stop = System.currentTimeMillis();
-                            System.out.println("Duration: %4d, Average:%4.2f".formatted((stop - start), ((stop - start) / 20.0)));
-                        }});}
-        try {TimeUnit.MINUTES.sleep(1);} catch (Exception e) {}
+                            System.out.println("Duration: %4d, Average:%4.2f".formatted((stop - start), ((double) (stop - start) / 20.0)));
+                        }
+                    });
+        });
+        try {
+            TimeUnit.MINUTES.sleep(1);
+        } catch (Exception e) {
+        }
     }
 }
